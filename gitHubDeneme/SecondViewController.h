dee52398a274b706acb5af9c74faa430c16d56b2
Baran Berk Kosker <kberkbaran@gmail.com>
2018-04-07 14:42:31 +0300

    secondController added

---
+++ gitHubDeneme/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  gitHubDeneme
+//
+//  Created by Baran Berk Kosker on 07/04/2018.
+//  Copyright © 2018 Mobiversite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
